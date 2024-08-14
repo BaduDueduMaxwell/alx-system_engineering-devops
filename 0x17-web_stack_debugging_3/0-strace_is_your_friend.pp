c12779277b7332e0ce5e7ccc3db2de968f68b4b1
walkingkode <duedumaxwell43@gmail.com>
2024-08-14 16:46:20 -0700

    task 1:strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,22 +1,10 @@
-# 0-strace_is_your_friend.pp
+# A puppet manuscript to replace a line in a file on a server
 
-file { '/etc/apache2/conf-available/missing.conf':
-  ensure  => file,
-  content => 'Your configuration content here',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-}
+$file_to_edit = '/var/www/html/wp-settings.php'
 
-exec { 'enable_missing_conf':
-  command     => '/usr/sbin/a2enconf missing.conf',
-  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-  refreshonly => true,
-  subscribes  => File['/etc/apache2/conf-available/missing.conf'],
-}
+#replace line containing "phpp" with "php"
 
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => Exec['enable_missing_conf'],
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
